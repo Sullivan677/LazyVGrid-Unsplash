9b17456584c5922a4c367480d2a5bfefc6bb2cda
Sullivan677 <decarli.sulli@gmail.com>
2021-02-15 14:16:52 +0100

    Initial Commit

---
+++ PictureGrid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PictureGrid
+//
+//  Created by Sullivan De carli on 15/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PictureGrid/PictureGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictureGridApp.swift
+//  PictureGrid
+//
+//  Created by Sullivan De carli on 15/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct PictureGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
